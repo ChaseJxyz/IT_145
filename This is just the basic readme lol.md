dcee3bce7e82e4041f089d72a10ab254cc8a498e
Chase J <85197722+ChaseJxyz@users.noreply.github.com>
2026-04-27 14:29:21 -0700

    basic hello world to set up enviornement

--- This is just the basic readme lol.md
+++ This is just the basic readme lol.md
@@ -1,1 +1,7 @@
-This is just the basic readme lol
+## SNHU IT-145 Foundations in Application Development
+
+Classwork/homework/projects/etc
+
+If you're another student, please don't plagiarize my code. You won't learn how to program if you don't do the work!
+
+I'm 100% new to Java, so please don't expect any of this to be groundbreaking code 👍️
---
+++ etc/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    // This is just a copy of hello world to test my IDE
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hitting the 'run' button will do all the compiling running stuff for me, hooray!");
+        System.err.println("This is 'error text' does it show up differently?");
+    }
+}
